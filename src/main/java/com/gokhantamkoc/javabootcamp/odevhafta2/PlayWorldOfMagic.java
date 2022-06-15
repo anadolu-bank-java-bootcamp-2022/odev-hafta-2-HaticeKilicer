a01a91568aec4e9eb90c4e37ae3204d6c2c416f1
haticekilicer <kilicer.hatice98@gmail.com>
2022-06-15 22:45:16 +0300

    Week2 homework finished!

--- src/main/java/com/gokhantamkoc/javabootcamp/odevhafta2/PlayWorldOfMagic.java
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta2/PlayWorldOfMagic.java
@@ -18,13 +18,9 @@ public class PlayWorldOfMagic implements CommandLineRunner {
 		float[] spellDamageInfo = createSpellDamageRepository();
 		String[] bossNames = createBossNameRepository();
 		float[] bossHps = createBossHPRepository();
-		
-		int minNumberSpellsUsed = resolveBattle(
-				magicianSpells, 
-				spellDamageInfo, 
-				bossNames, 
-				bossHps);
-		
+
+		int minNumberSpellsUsed = resolveBattle(magicianSpells, spellDamageInfo, bossNames, bossHps);
+
 		if (minNumberSpellsUsed > maxNumOfAttacksAllowed) {
 			System.out.println("Magician died!");
 		} else if (minNumberSpellsUsed > 0 && minNumberSpellsUsed <= maxNumOfAttacksAllowed) {
@@ -33,37 +29,56 @@ public class PlayWorldOfMagic implements CommandLineRunner {
 			System.out.println("Result is not correct!");
 		}
 	}
-	
-	public static int resolveBattle(
-			String[] magicianSpells,
-			float[] spellDamageInfo,
-			String[] bossNames,
-			float[] bossHPs
-			) {
-		
+
+	public static int resolveBattle(String[] magicianSpells, float[] spellDamageInfo, String[] bossNames,
+			float[] bossHPs) {
+
 		int spellsUsed = 0;
 		// ______ BASLANGIC _______ Kodunuz buradan baslamali
-		
+
 		// ______ SON _______ Kodunuz burada bitmeli
-		/* NOT: ______ BASLANGIC _______ ve ______ SON _______ 
-		 * arasina istediginiz kadar sayida satir ekleyebilirsiniz.
+		/*
+		 * NOT: ______ BASLANGIC _______ ve ______ SON _______ arasina istediginiz kadar
+		 * sayida satir ekleyebilirsiniz.
 		 */
-		return spellsUsed;
+
+		// find max damage
+		float maxDamage = 0;
+		for (int index = 0; index < spellDamageInfo.length; index++) {
+			if (spellDamageInfo[index] > maxDamage)
+				maxDamage = spellDamageInfo[index];
+		}
+
+		System.out.println("maxDamage: " + maxDamage);
+
+		// spell count
+		int spellCount = 0;
+		float remHPs = 0f; // remainder HPs
+
+		for (int index = 0; index < bossHPs.length; index++) {
+			remHPs = bossHPs[index];
+			while ((int) remHPs > 0) {
+
+				remHPs = remHPs - maxDamage;
+				spellCount++;
+			}
+		}
+		return spellsUsed = spellCount;
 	}
-	
+
 	public static String[] createSpellNameRepository() {
-		return new String[]{"Ice Storm", "Chain Lightning", "Magic Missile"};
+		return new String[] { "Ice Storm", "Chain Lightning", "Magic Missile" };
 	}
-	
+
 	public static float[] createSpellDamageRepository() {
-		return new float[]{40.0f, 30.0f, 5.0f};
+		return new float[] { 40.0f, 30.0f, 5.0f };
 	}
-	
+
 	public static String[] createBossNameRepository() {
-		return new String[]{"Dire Rat", "Skeleton Knight", "Undead King"};
+		return new String[] { "Dire Rat", "Skeleton Knight", "Undead King" };
 	}
-	
+
 	public static float[] createBossHPRepository() {
-		return new float[]{15.0f, 45.0f, 60.0f};
+		return new float[] { 15.0f, 45.0f, 60.0f };
 	}
 }
